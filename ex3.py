bb2e50058f420db2d65f2c6268790585053d1409
Mark Yu <markedward82@msn.com>
2019-07-18 16:02:40 -0700

    ex3.py

---
+++ ex3.py
@@ -0,0 +1,39 @@
+#print out I will now count my chickens
+print("I will now count my chickens")
+
+#print out string Hens and the number of hens 
+print("Hens", 25+30/6)
+
+#print out roosters, and the number of roosters 
+print("Roosters", 100 - 25 *3 % 4)
+
+#print out string Now I will count the eggs
+print("Now I will count the eggs:")
+
+#print out the numbers of egg
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#print out string is it true that 3 + 2 < 5 - 7
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#print out False due to the < operator
+print(3 + 2 < 5 - 7)
+
+#print out string what is 3 + 2? and the answer
+print("What is 3 + 2?",  3 + 2)
+#print out string what is 5 - 7? and the result
+print("What is 5 - 7?", 5 -7)
+
+#print out string oh, that's why it's false
+print("Oh, that's why it's False.")
+#print out string how about some mroe.
+print("How about some more.")
+
+#print out string is it great? and give a boolean
+print("Is it great?", 5 > -2)
+#print out string is it greater or equal? and gives a boolean
+print("Is it greater or equal?", 5 >= -2)
+#print out string is it less or equal? and gives a boolean
+print("Is it less or equal?", 5 <= -2)
+
+
