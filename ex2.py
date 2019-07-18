d5a0165a5a2f6e26083b68ab62810c02fc902e8d
Mark Yu <markedward82@msn.com>
2019-07-18 15:28:24 -0700

    ex2

--- ex2.py
+++ ex2.py
@@ -6,6 +6,12 @@ print("I could have code like this.") # and the comment after is ignored
 # You can also use a comment to "disable" or comment out a piece of code:
 # print "This won't run."
 
-print("This will run")
+print("This will run.")
+
+# extra credit
+# 1. Find out if you were right about what the # character does and make sure you know what's call(octothorpe or pound character)
+# 2. Take your ex2.py file and review each line going bakcwards. Start at the last line, and chek word in reverse against what you should have typed
+# 3. Did you find more mistakes? Fix them.
+# 4. Read what you typed above out loud, including saying each character by its name. Did you find more mistakes? Fix them.
 
 
