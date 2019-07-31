34d0f60da600a718d9cd7ff9a190d5685c7bdde9
markedward82 <markedward82@msn.com>
2019-07-30 19:47:23 -0700

    ex12

---
+++ ex12.py
@@ -0,0 +1,10 @@
+#prompting people
+
+#input take a user action, and save the string to y
+y = input("name?")
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How fat are you?")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
