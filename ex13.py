6d324ba9731714d039bda8c9b5e1d832ad21a0d6
Mark Yu <markedward82@msn.com>
2019-08-01 10:55:58 -0700

    ex13.py

---
+++ ex13.py
@@ -0,0 +1,30 @@
+#ex13: parameter, unpacking, variable
+
+
+
+#import package from sys call argv
+from sys import argv
+
+#argv is arguement variable 
+
+#set argv to script, first, second, third variables
+script, first, second, third = argv
+
+#display the variables
+print("the script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+
+#test my own creation
+a, b, c , d = argv
+
+#call the script
+print("Testing my code:", a)
+print("This is b:", b)
+print("this is c:", c)
+print("this is d:", d)
+
+
+
